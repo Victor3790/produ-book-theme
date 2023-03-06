b0b8028378ae59d0600912db141063885a409ecd
victor <victor3790@gmail.com>
2023-03-05 20:44:58 -0600

    Bug in search form fixed

--- searchform.php
+++ searchform.php
@@ -10,7 +10,7 @@
 <div class="card mb-4">
 	<div class="card-header">Search</div>
 	<div class="card-body">
-		<form action="<?php home_url(); ?>">
+		<form action="<?php echo esc_url( home_url() ); ?>">
 			<div class="input-group">
 				<input class="form-control" type="text" placeholder="Enter search term..." aria-label="Enter search term..." aria-describedby="button-search" name="s" id="s" />
 				<input type="submit" class="btn btn-primary" value="Go!" id="button-search">
