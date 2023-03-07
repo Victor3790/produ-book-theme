492d8afc028f40b2cf4300d6a65277e985e3de93
victor <victor3790@gmail.com>
2023-03-07 11:08:50 -0600

    Done

--- single-books.php
+++ single-books.php
@@ -30,6 +30,42 @@
 
 			?>
 
+			<h3 class="my-5">También te puede interesar</h3>
+			<div class="row">
+			<?php
+
+			$categories    = get_the_terms( $post, 'book_categories' );
+			$book_category = $categories[0]->name;
+
+			$args = array(
+				'post_type'      => 'books',
+				'post__not_in'   => array( $post->ID ),
+				'posts_per_page' => 2,
+				//phpcs:ignore
+				'tax_query' => array(
+					array(
+						'taxonomy' => 'book_categories',
+						'field'    => 'slug',
+						'terms'    => $book_category,
+					),
+				),
+			);
+
+			$related_books_query = new WP_Query( $args );
+
+			if ( $related_books_query->have_posts() ) {
+
+				while ( $related_books_query->have_posts() ) {
+
+					$related_books_query->the_post();
+
+					get_template_part( 'template-parts/card', 'book' );
+
+				}
+			}
+
+			?>
+			</div>
 		</div>
 		<!-- Side widgets-->
 		<div class="col-lg-4">
